0860b6cbd877eeb65811815c92f0ba0ac86aad62
Rachel Wang <rswang@mit.edu>
2014-03-29 19:04:38 -0400

    Add search post...

--- login/app/server/router.js
+++ login/app/server/router.js
@@ -23,23 +23,11 @@ module.exports = function(app) {
 				}
 			});
 		}
+
 	});
 
-	app.post('/', function(req, res){
-	// check if the user's credentials are saved in a cookie //
-		if (req.cookies.user == undefined || req.cookies.pass == undefined){
-			res.render('index', { title: 'Hello - Please Login To Your Account' });
-		}	else{
-	// attempt automatic login //
-			AM.autoLogin(req.cookies.user, req.cookies.pass, function(o){
-				if (o != null){
-				    req.session.user = o;
-					res.redirect('/home');
-				}	else{
-					res.render('login', { title: 'Hello - Please Login To Your Account' });
-				}
-			});
-		}
+	app.post('/', function(req,res) {
+		console.log('hi');
 	});
 
 	app.get('/login', function(req, res){
